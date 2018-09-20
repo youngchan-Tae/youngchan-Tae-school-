186a57e8d0038f74561ba4eff1912b5611e153b9
youngchan-Tae <1140116@naver.com>
2018-09-20 17:09:31 +0900

    2015182043 태영찬
    마우스 움직임 및 이벤트 구현

--- drill6.py
+++ drill6.py
@@ -3,6 +3,28 @@ from pico2d import *
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
+
+def handle_events():
+    global running
+    global x, y
+    global mouse_x, mouse_y
+    global frame
+    global mx, my
+    count = 0
+
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            mx, my = event.x, KPU_HEIGHT - 1 - event.y
+            mouse.clip_draw(0, 0, 100, 100, mx, my)
+            update_canvas()
+
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 
 kpu_ground = load_image('KPU_GROUND.png')
@@ -21,9 +43,6 @@ while running:
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     mouse.clip_draw(0, 0, 100, 100, mx, my)
     update_canvas()
+    handle_events()
 
 close_canvas()
-
-
-
-
